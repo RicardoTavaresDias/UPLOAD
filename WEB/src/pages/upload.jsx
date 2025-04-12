d84e949db4bc471c0bb91e97cecc30798de6a8ca
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-04-12 14:52:50 -0300

    versão 2.6.12

--- WEB/src/pages/upload.jsx
+++ WEB/src/pages/upload.jsx
@@ -1,6 +1,7 @@
 import { InputUpload } from "../components/input-upload.jsx"
 import { UploadLoading } from '../components/upload-loading.jsx';
 import { Button } from "../components/button.jsx";
+import svg from '../assets/close.svg'
 
 import { useUpload } from "../hooks/useUpload.js";
 
@@ -35,14 +36,16 @@ export function Upload(){
       <InputUpload>
         <input type="file" id="birth-file" name="birth-file" onChange={(e) => addUpload(e.target.files[0])} />
       </InputUpload>
+
       {file && file.map((value, index) => (
         <UploadLoading key={value.id} fileName={value.file?.name} progresse={progress} border={error} onClick={() => get()}>
           <a href="#" onClick={() => handleRemove(value.id)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+            <img src={svg} />
           </a>
         </UploadLoading>
       ))
       }
+      
       <Button >
         <button className="btn-primary" type="submit" onClick={onSubmit}>
           Upload
