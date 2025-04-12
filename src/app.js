d247adbfe36fd65f7360716d45ff089a246fbff2
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-04-12 06:27:24 -0300

    versão 1.4.7

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import "express-async-errors"
 import express from "express"
 import { router } from "./routes/router.js"
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 export const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 // get dos arquivos da pasta - tmp/document || tmp/upload
 app.use('/document', express.static(`${process.env.FILE_SERVER_PATH}/document`))
